2baa775b9f51ecf30c96f36e322381b49fcad8bf
edgar1548 <edgar.chacon@utec.edu.pe>
2023-06-16 01:10:46 -0500

    general schema 2

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from flask import Flask
-
-from models.pub_key import PublicKeys
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db= SQLAlchemy(app)
+
+
+class Profiles(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(254))
+    public_key = db.Column(db.String(254))
+    def __init__(self, email, public_key) -> None:
+        self.email = email
+        self.public_key = public_key
 
 @app.route('/')
 def get_keys():
@@ -11,8 +26,8 @@ def get_keys():
 @app.route('/', methods = ['POST'])
 def add_key():
     return ''
-
+    
 @app.route('/verify')
 def test_keys():
-    return True
+    return ''
 
--- models/pub_key.py
+++
@@ -1,6 +0,0 @@
-
-
-class PublicKeys():
-    def __init__(self, email, public_key):
-        self.email = email
-        self.public_key = public_key
